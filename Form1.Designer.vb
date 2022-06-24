da60d2b20250729b3b817e4b0e7a8a6ec3436fce
kuganraj97 <75333632+kuganraj97@users.noreply.github.com>
2022-06-24 16:48:16 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.UserIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.HeightDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WeightDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AgeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BmrDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BmiDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TddeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CalBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CalDataSet = New OOP_2.CalDataSet()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.CalTableAdapter = New OOP_2.CalDataSetTableAdapters.CalTableAdapter()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.TextEdit2 = New DevExpress.XtraEditors.TextEdit()
+        Me.TextEdit3 = New DevExpress.XtraEditors.TextEdit()
+        Me.TextEdit4 = New DevExpress.XtraEditors.TextEdit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CalBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CalDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit3.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit4.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(44, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Height"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Weight"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Name"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.UserIDDataGridViewTextBoxColumn, Me.HeightDataGridViewTextBoxColumn, Me.WeightDataGridViewTextBoxColumn, Me.AgeDataGridViewTextBoxColumn, Me.BmrDataGridViewTextBoxColumn, Me.BmiDataGridViewTextBoxColumn, Me.TddeDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.CalBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(349, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(744, 280)
+        Me.DataGridView1.TabIndex = 20
+        '
+        'UserIDDataGridViewTextBoxColumn
+        '
+        Me.UserIDDataGridViewTextBoxColumn.DataPropertyName = "UserID"
+        Me.UserIDDataGridViewTextBoxColumn.HeaderText = "UserID"
+        Me.UserIDDataGridViewTextBoxColumn.Name = "UserIDDataGridViewTextBoxColumn"
+        '
+        'HeightDataGridViewTextBoxColumn
+        '
+        Me.HeightDataGridViewTextBoxColumn.DataPropertyName = "Height"
+        Me.HeightDataGridViewTextBoxColumn.HeaderText = "Height"
+        Me.HeightDataGridViewTextBoxColumn.Name = "HeightDataGridViewTextBoxColumn"
+        '
+        'WeightDataGridViewTextBoxColumn
+        '
+        Me.WeightDataGridViewTextBoxColumn.DataPropertyName = "Weight"
+        Me.WeightDataGridViewTextBoxColumn.HeaderText = "Weight"
+        Me.WeightDataGridViewTextBoxColumn.Name = "WeightDataGridViewTextBoxColumn"
+        '
+        'AgeDataGridViewTextBoxColumn
+        '
+        Me.AgeDataGridViewTextBoxColumn.DataPropertyName = "Age"
+        Me.AgeDataGridViewTextBoxColumn.HeaderText = "Age"
+        Me.AgeDataGridViewTextBoxColumn.Name = "AgeDataGridViewTextBoxColumn"
+        '
+        'BmrDataGridViewTextBoxColumn
+        '
+        Me.BmrDataGridViewTextBoxColumn.DataPropertyName = "bmr"
+        Me.BmrDataGridViewTextBoxColumn.HeaderText = "bmr"
+        Me.BmrDataGridViewTextBoxColumn.Name = "BmrDataGridViewTextBoxColumn"
+        '
+        'BmiDataGridViewTextBoxColumn
+        '
+        Me.BmiDataGridViewTextBoxColumn.DataPropertyName = "bmi"
+        Me.BmiDataGridViewTextBoxColumn.HeaderText = "bmi"
+        Me.BmiDataGridViewTextBoxColumn.Name = "BmiDataGridViewTextBoxColumn"
+        '
+        'TddeDataGridViewTextBoxColumn
+        '
+        Me.TddeDataGridViewTextBoxColumn.DataPropertyName = "Tdde"
+        Me.TddeDataGridViewTextBoxColumn.HeaderText = "Tdde"
+        Me.TddeDataGridViewTextBoxColumn.Name = "TddeDataGridViewTextBoxColumn"
+        '
+        'CalBindingSource
+        '
+        Me.CalBindingSource.DataMember = "Cal"
+        Me.CalBindingSource.DataSource = Me.CalDataSet
+        '
+        'CalDataSet
+        '
+        Me.CalDataSet.DataSetName = "CalDataSet"
+        Me.CalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "Age"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 219)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(70, 13)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "Activity Level"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(221, 269)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(118, 269)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 27
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'CalTableAdapter
+        '
+        Me.CalTableAdapter.ClearBeforeFill = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(102, 211)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(227, 21)
+        Me.ComboBox1.TabIndex = 28
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.Location = New System.Drawing.Point(102, 161)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Properties.Mask.EditMask = "[01]+"
+        Me.TextEdit1.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx
+        Me.TextEdit1.Size = New System.Drawing.Size(227, 20)
+        Me.TextEdit1.TabIndex = 29
+        '
+        'TextEdit2
+        '
+        Me.TextEdit2.Location = New System.Drawing.Point(102, 121)
+        Me.TextEdit2.Name = "TextEdit2"
+        Me.TextEdit2.Properties.Mask.EditMask = "\d+"
+        Me.TextEdit2.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx
+        Me.TextEdit2.Size = New System.Drawing.Size(227, 20)
+        Me.TextEdit2.TabIndex = 30
+        '
+        'TextEdit3
+        '
+        Me.TextEdit3.Location = New System.Drawing.Point(102, 80)
+        Me.TextEdit3.Name = "TextEdit3"
+        Me.TextEdit3.Properties.AllowNullInput = DevExpress.Utils.DefaultBoolean.[False]
+        Me.TextEdit3.Properties.Mask.EditMask = "\d+"
+        Me.TextEdit3.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx
+        Me.TextEdit3.Size = New System.Drawing.Size(227, 20)
+        Me.TextEdit3.TabIndex = 31
+        '
+        'TextEdit4
+        '
+        Me.TextEdit4.Location = New System.Drawing.Point(102, 36)
+        Me.TextEdit4.Name = "TextEdit4"
+        Me.TextEdit4.Properties.AllowNullInput = DevExpress.Utils.DefaultBoolean.[False]
+        Me.TextEdit4.Properties.Mask.EditMask = "\p{L}+"
+        Me.TextEdit4.Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx
+        Me.TextEdit4.Size = New System.Drawing.Size(227, 20)
+        Me.TextEdit4.TabIndex = 32
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1139, 325)
+        Me.Controls.Add(Me.TextEdit4)
+        Me.Controls.Add(Me.TextEdit3)
+        Me.Controls.Add(Me.TextEdit2)
+        Me.Controls.Add(Me.TextEdit1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CalBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CalDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit3.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit4.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents CalDataSet As OOP_2.CalDataSet
+    Friend WithEvents CalBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents CalTableAdapter As OOP_2.CalDataSetTableAdapters.CalTableAdapter
+    Friend WithEvents UserIDDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents HeightDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents WeightDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents AgeDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BmrDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BmiDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TddeDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextEdit1 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TextEdit2 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TextEdit3 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents TextEdit4 As DevExpress.XtraEditors.TextEdit
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    'Connect DB
+    Dim connection As SqlConnection = New SqlConnection("Data Source=DESKTOP-PIHCL6D\MYSQLSERVER2;Initial Catalog=Cal;Integrated Security=True")
+
+    'Class
+    Public Class user
+        Public UserID As String
+        Public Weight As Decimal
+        Public Height As Decimal
+        Public Age As Integer
+        Public Tdde As Decimal
+
+
+        'Method 1
+        Public Function Getbmi(ByVal Height, ByVal Weight)
+            Dim bmi As Decimal = (Weight / (Height * Height)) * 10000
+            Return bmi
+        End Function
+
+        'Method 2
+        Public Function GetBmr(ByVal Height, ByVal Age, ByVal Weight)
+            Dim bmr As Decimal = (88.362 + (13.397 * Weight) + (4.799 * Height) - (5.677 * Age))
+            Return bmr
+        End Function
+
+    End Class
+
+    Public Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        ' insert
+        Dim human As CalDataSet.CalRow = Me.CalDataSet.Cal.NewRow()
+        human.UserID = TextEdit1.Text
+        human.Height = TextEdit3.Text
+        human.Weight = TextEdit2.Text
+        human.Age = TextEdit4.Text
+        human.bmi = (human.Weight / (human.Height * human.Height)) * 10000
+        human.bmr = (88.362 + (13.397 * human.Weight) + (4.799 * human.Height) - (5.677 * human.Age))
+        Dim Tdde As Decimal
+
+        If ComboBox1.SelectedItem = "Sedentary" Then
+            Tdde = ((88.362 + (13.397 * human.Weight) + (4.799 * human.Height) - (5.677 * human.Age)) * 1.2)
+        ElseIf ComboBox1.SelectedItem = "Lightly Active" Then
+            Tdde = ((88.362 + (13.397 * human.Weight) + (4.799 * human.Height) - (5.677 * human.Age)) * 1.375)
+        ElseIf ComboBox1.SelectedItem = "Moderately Active" Then
+            Tdde = ((88.362 + (13.397 * human.Weight) + (4.799 * human.Height) - (5.677 * human.Age)) * 1.55)
+        Else
+            Tdde = ((88.362 + (13.397 * human.Weight) + (4.799 * human.Height) - (5.677 * human.Age)) * 1.75)
+        End If
+        human.Tdde = Tdde
+        Me.CalDataSet.Cal.Rows.Add(human)
+        Me.CalTableAdapter.Update(Me.CalDataSet)
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CalDataSet.Cal' table. You can move, or remove it, as needed.
+        Me.CalTableAdapter.Fill(Me.CalDataSet.Cal)
+        ComboBox1.Items.Add("Sedentary")
+        ComboBox1.Items.Add("Lightly Active")
+        ComboBox1.Items.Add("Moderately Active")
+        ComboBox1.Items.Add("Very Active")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        'Save
+        Me.CalTableAdapter.Update(Me.CalDataSet)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        'Delete
+        Dim i As Integer = DataGridView1.CurrentRow.Index
+        Me.CalDataSet.Cal.Rows(i).Delete()
+        Me.CalTableAdapter.Update(Me.CalDataSet)
+    End Sub
+
+    Private Sub TextEdit4_EditValueChanged(sender As System.Object, e As System.EventArgs) Handles TextEdit4.EditValueChanged
+
+    End Sub
+End Class
